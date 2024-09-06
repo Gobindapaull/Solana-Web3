7b73fd1509dd8e46bdf18de4a068e820eba5967a
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2024-09-06 23:55:44 +0530

    Create buysell.js

---
+++ PUMPFUN/buysell/buysell.js
@@ -0,0 +1,78 @@
+import { VersionedTransaction, Connection, Keypair } from '@solana/web3.js';
+import bs58 from "bs58";
+
+const RPC_ENDPOINT = "https://api.mainnet-beta.solana.com";
+const web3Connection = new Connection(
+    RPC_ENDPOINT,
+    'confirmed',
+);
+
+async function buy() {
+    const response = await fetch(`https://pumpportal.fun/api/trade-local`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            "publicKey": "4RFnGqwoQzHe38ZCpsHrGfcxDFvDhsTCGak5RMpuXqvT",  // Your wallet public key
+            "action": "buy",                 // "buy" or "sell"
+            "mint": "BTx2uAma11W2UGMnehnChnN1HAoMzJ3mFVF2AeVqpump",         // contract address of the token you want to trade
+            "denominatedInSol": "false",     // "true" if amount is amount of SOL, "false" if amount is number of tokens
+            "amount": 1000,                  // amount of SOL or tokens
+            "slippage": 10,                  // percent slippage allowed
+            "priorityFee": 0.00001,          // priority fee
+            "pool": "pump"                   // exchange to trade on. "pump" or "raydium"
+        })
+    });
+    if (response.status === 200) { // successfully generated transaction
+        const data = await response.arrayBuffer();
+        const tx = VersionedTransaction.deserialize(new Uint8Array(data));
+        const signerKeyPair = Keypair.fromSecretKey(bs58.decode(""));
+        tx.sign([signerKeyPair]);
+        const signature = await web3Connection.sendTransaction(tx)
+        console.log("[bought] -- Transaction: https://solscan.io/tx/" + signature);
+    } else {
+        console.log(response.statusText); // log error
+    }
+}
+
+async function sell() {
+    const response = await fetch(`https://pumpportal.fun/api/trade-local`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            "publicKey": "4RFnGqwoQzHe38ZCpsHrGfcxDFvDhsTCGak5RMpuXqvT",  // Your wallet public key
+            "action": "sell",                 // "buy" or "sell"
+            "mint": "BTx2uAma11W2UGMnehnChnN1HAoMzJ3mFVF2AeVqpump",         // contract address of the token you want to trade
+            "denominatedInSol": "false",     // "true" if amount is amount of SOL, "false" if amount is number of tokens
+            "amount": 2000,                  // amount of SOL or tokens
+            "slippage": 10,                  // percent slippage allowed
+            "priorityFee": 0.00001,          // priority fee
+            "pool": "pump"                   // exchange to trade on. "pump" or "raydium"
+        })
+    });
+    if (response.status === 200) { // successfully generated transaction
+        const data = await response.arrayBuffer();
+        const tx = VersionedTransaction.deserialize(new Uint8Array(data));
+        const signerKeyPair = Keypair.fromSecretKey(bs58.decode(""));
+        tx.sign([signerKeyPair]);
+        const signature = await web3Connection.sendTransaction(tx)
+        console.log("[sold] -- Transaction: https://solscan.io/tx/" + signature);
+    } else {
+        console.log(response.statusText); // log error
+    }
+}
+
+async function sendPortalTransaction() {
+    await buy()
+
+    setTimeout(() => {
+        console.log("Waited for 3 seconds");
+    }, 3000);
+    
+    await sell()
+}
+
+sendPortalTransaction();
