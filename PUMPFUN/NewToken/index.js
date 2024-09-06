626284559fd680316950b31a54a636fececdcaad
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2024-09-07 01:07:54 +0530

    Create index.js

---
+++ PUMPFUN/NewToken/index.js
@@ -0,0 +1,21 @@
+import WebSocket from "ws"
+const ws = new WebSocket("wss://pumpportal.fun/api/data")
+
+ws.on('open', (open) => {
+    let payload = {
+        method: "subscribeNewToken"
+    }
+    ws.send(JSON.stringify(payload))
+
+})
+
+ws.on('message', (data) => {
+    const mint = JSON.parse(data)
+    Object.keys(mint).map(key => {
+        if (key == "mint") {
+            const newPumpFunTokenAddress = mint[key]
+            console.log(newPumpFunTokenAddress)
+        }
+    }
+    )
+})
