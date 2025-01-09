994217172a6dd4e2244a2f57fcc65227da4c3198
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2025-01-09 10:36:35 +0530

    Update swap.ts

--- jup/swap.ts
+++ jup/swap.ts
@@ -2,9 +2,9 @@ import { Connection, Keypair, VersionedTransaction, clusterApiUrl } from '@solan
 import bs58 from 'bs58';
 
 const inputMint = 'So11111111111111111111111111111111111111112';
-const outputMint = '7GCihgDB8fe6KNjn2MYtkzZcRjQy3t9GHdC8uHYmW2hr';
-const inputAmount = 0.00001;
-const slippage = 50;
+const outputMint = 'EbvJsEaRfoP7dgvegCUwVqE7jNZ8YPtNgDtNuDE1pump';
+const inputAmount = 0.0001;
+const slippage = 300;
 
 const wallet = Keypair.fromSecretKey(new Uint8Array(bs58.decode("")));
 console.log(wallet.publicKey.toBase58());
@@ -24,8 +24,7 @@ const { swapTransaction } = await (
     },
     body: JSON.stringify({
       quoteResponse,
-      userPublicKey: wallet.publicKey.toString(),
-      wrapAndUnwrapSol: true,
+      userPublicKey: wallet.publicKey.toString()
     })
   })
 ).json();
@@ -40,9 +39,12 @@ transaction.sign([wallet]);
 
 // Execute the transaction
 const rawTransaction = transaction.serialize()
-const txid = await connection.sendRawTransaction(rawTransaction, {
-  skipPreflight: true,
-  maxRetries: 2
-});
-await connection.confirmTransaction(txid);
+const txid = await connection.sendRawTransaction(rawTransaction);
+
+const result = await connection.confirmTransaction(txid);
+console.log(result);
 console.log(`https://solscan.io/tx/${txid}`);
+
+// https://solscan.io/tx/5NiXqJ9w5U66M4xf9bcrLiWNfjArfCmPXVU4JfDtt4v2NVQ58gvLnzJJhYuLRcRYSU1mibJKc9m6UgtwdfsDwMFa
+// https://solscan.io/tx/3wxEraFyLwxCJCM2ye4n3MJVVPZniPWSXfqYLYKpEBfKBQKn8yN7tLNtNpDh3WqGN6LL6xFebz6f2JUhh5dBDwVP
+// https://solscan.io/tx/nVPMPeENs1RK9pE4JpggwiKV7R85yM1eNUxs1sGvuaneKFEGQDAvmBo4VJP3yviRm1GuKBWUzgvRjPbZLN1QeBP
