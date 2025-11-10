eeb9d2375a21ba650d5bd7b7485f8bf97adb75e0
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2025-11-10 15:15:02 +0530

    Create page.tsx

---
+++ MetadataViewer/app/page.tsx
@@ -0,0 +1,9 @@
+import TokenInfo from "./components/TokenInfo";
+
+export default function Home() {
+  return (
+<main className="min-h-screen flex items-center justify-center bg-gray-100">
+      <TokenInfo />
+    </main>
+  );
+}
